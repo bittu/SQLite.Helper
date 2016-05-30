d8394eeeb64e4ca1b1de5df875dbba56314d6bef
Hugo Rodrigues <hugorodrigues@openmailbox.org>
2016-05-30 14:11:37 +0100

    Updated some code

--- SQLiteHelper.cs
+++ SQLiteHelper.cs
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with SQLite.Helper.  If not, see <http://www.gnu.org/licenses/>.
 */
+using System;
 using System.Data;
 using System.Data.SQLite;
 using System.IO;
@@ -25,175 +26,126 @@ namespace SQLite.Helper
     /// </summary>
     public class SQLiteHelper
     {
+        private readonly string ConnectionString;
+
         /// <summary>
         /// Connection to the SQLite database
         /// </summary>
-        public SQLiteConnection Connection { get; set; }
+        [Obsolete("Connections are created when needed", true)]
+        public SQLiteConnection Connection { get; private set; }
 
         /// <summary>
         /// If and general error has occurred 
         /// </summary>
+        [Obsolete("Use the exception that the methods throw", true)]
         public bool GeneralError { get; private set; }
 
         /// <summary>
-        /// If and sql error has occured 
+        /// If and sql error has occurred 
         /// </summary>
+        [Obsolete("Use the exception that the methods throw", true)]
         public bool SqlError { get; private set; }
 
         /// <summary>
         /// Error message
         /// </summary>
+        [Obsolete("Use the exception that the methods throw", true)]
         public string SqlErrorMessage { get; private set; }
 
         /// <summary>
         /// SQLite error number 
         /// </summary>
+        [Obsolete("Use the exception that the methods throw", true)]
         public int SqlErrorNum { get; private set; }
 
         /// <summary>
         /// Doesn't create a connection
         /// </summary>
+        [Obsolete("You must pass the connection, or the file", true)]
         public SQLiteHelper()
         {
-            GeneralError = false;
-            SqlError = false;
-            SqlErrorMessage = "";
-            SqlErrorNum = 0;
         }
-        
+
         /// <summary>
         /// Checks if the database exists and creates the connnection to the database
         /// </summary>
-        /// <param name="__Database">Database Path</param>
-        public SQLiteHelper(string __Database)
+        /// <param name="DatabasePath">Database Path</param>
+        public SQLiteHelper(string DatabasePath)
         {
-            GeneralError = false;
-            SqlError = false;
-            SqlErrorMessage = "";
-            SqlErrorNum = 0;
-
             // Checks if the database exists
-            if (!File.Exists(__Database))
+            if (!File.Exists(DatabasePath))
             {
-                GeneralError = true;
+                throw new FileNotFoundException("Database not found", DatabasePath);
             }
             else
             {
                 // Creates the connection
-                Connection = new SQLiteConnection("Data Source=" + __Database + "; Version=3");
+                ConnectionString = "Data Source=" + DatabasePath + "; Version=3";
             }
         }
 
         /// <summary>
         /// Creates a database
         /// </summary>
-        /// <param name="__Sql">SQL statement</param>
-        /// <param name="__DropSql">SQL to drop current tables</param>
-        public void CreateDb(string __Sql, string __DropSql = null)
+        /// <param name="Query">SQL statement</param>
+        public void CreateDb(string Query)
         {
-            // Check if occurred any error
-            if (GeneralError)
-            {
-                return;
-            }
+            this.SendQuery(Query);
+        }
 
-            SQLiteCommand SqlCmd = null;
-            try
-            {
-                Connection.Open();
-
-                // Drop the current values
-                if (__DropSql != null)
-                {
-                    SqlCmd = new SQLiteCommand(__DropSql, Connection);
-                    SqlCmd.ExecuteNonQuery();
-                    SqlCmd.Dispose();
-                }
-
-                SqlCmd = new SQLiteCommand(__Sql, Connection);
-                SqlCmd.ExecuteNonQuery();
-                SqlError = false;
-            }
-            catch (SQLiteException error)
-            {
-                SqlErrorMessage = error.Message.ToString();
-                SqlErrorNum = error.ErrorCode;
-                SqlError = true;
-            }
-            finally
-            {
-                SqlCmd.Dispose();
-                Connection.Close();
-            }
+        /// <summary>
+        /// Creates a database
+        /// </summary>
+        /// <param name="Query">SQL statement</param>
+        /// <param name="DropQuery">SQL to drop current tables</param>
+        [Obsolete("The query to drop tables no longer exists", false)]
+        public void CreateDb(string Query, string DropQuery = null)
+        {
+            this.CreateDb(Query);
         }
 
         /// <summary>
         /// Sends a SQL query that doesn't return any value
         /// It can also be used to create a table, but it's recomended to use CreateDb instead
         /// </summary>
-        /// <param name="__Sql">SQL statement</param>
-        public void SendQuery(string __Sql)
+        /// <param name="Query">SQL statement</param>
+        public void SendQuery(string Query)
         {
-
-            // Check if occurred any error
-            if (GeneralError)
+            using (SQLiteConnection Connection = new SQLiteConnection(this.ConnectionString))
             {
-                return;
-            }
-
-            SQLiteCommand SqlCmd = null;
-            try
-            {
-                Connection.Open();
-                SqlCmd = new SQLiteCommand(__Sql, Connection);
-                SqlCmd.ExecuteNonQuery();
-                SqlError = false;
-            }
-            catch (SQLiteException error)
-            {
-                SqlErrorMessage = error.Message.ToString();
-                SqlErrorNum = error.ErrorCode;
-                SqlError = true;
-            }
-            finally
-            {
-                SqlCmd.Dispose();
-                Connection.Close();
+                using (SQLiteCommand SqlCmd = new SQLiteCommand())
+                {
+                    SqlCmd.Connection = Connection;
+                    SqlCmd.CommandText = Query;
+                    Connection.Open();
+                    SqlCmd.ExecuteNonQuery();
+                }
             }
         }
 
         /// <summary>
         /// Gets the first result from the query
         /// </summary>
-        /// <param name="__sql">SQL statement</param>
-        /// <returns>Return the value inside a System.Object and must be converted</returns>
-        public object Get(string __sql)
+        /// <param name="Query">SQL statement</param>
+        /// <returns>Return the value inside a System.Object and must be converted. If no result, returns null</returns>
+        public object Get(string Query)
         {
-            if (GeneralError)
-            {
-                return null;
-            }
-
             object result = null;
-            SQLiteCommand SqlCmd = null;
-
-            try
+            using (SQLiteConnection Connection = new SQLiteConnection(this.ConnectionString))
             {
-                Connection.Open();
-                SqlCmd = new SQLiteCommand(__sql, Connection);
-                result = SqlCmd.ExecuteScalar();
-                SqlError = false;
-            }
-            catch (SQLiteException error)
-            {
-                SqlErrorMessage = error.Message.ToString();
-                SqlErrorNum = error.ErrorCode;
-                SqlError = true;
-            }
-            finally
-            {
-                SqlCmd.Dispose();
-                Connection.Close();
+                using (SQLiteCommand SqlCmd = new SQLiteCommand())
+                {
+                    SqlCmd.Connection = Connection;
+                    SqlCmd.CommandText = Query;
+                    Connection.Open();
+                    using (SQLiteDataReader Reader = SqlCmd.ExecuteReader())
+                    {
+                        if (Reader.NextResult())
+                        {
+                            result = Reader;
+                        }
+                    }
+                }
             }
 
             return result;
@@ -202,36 +154,21 @@ namespace SQLite.Helper
         /// <summary>
         /// Gets table values inside a dataset
         /// </summary>
-        /// <param name="__sql">SQL statement</param>
-        /// <param name="__table">Name of the table</param>
-        /// <returns>All the returned value into a dataset</returns>
-        public DataSet GetTable(string __sql, string __table)
+        /// <param name="Query">SQL statement</param>
+        /// <param name="TableName">Name of the table</param>
+        /// <returns>All the returned values into a dataset</returns>
+        public DataSet GetTable(string Query, string TableName)
         {
-            // Check if occurred any error
-            if (GeneralError)
+            DataSet data = null;
+
+            using (SQLiteConnection Connection = new SQLiteConnection(this.ConnectionString))
             {
-                return null;
+                using (SQLiteDataAdapter Adapter = new SQLiteDataAdapter(Query, Connection))
+                {
+                    Adapter.Fill(data, TableName);
+                }
             }
 
-            DataSet data = null;
-            SQLiteDataAdapter SqlAdapter = null;
-            try
-            {
-                SqlAdapter = new SQLiteDataAdapter(__sql, Connection);
-                SqlAdapter.Fill(data, __table);
-                SqlError = false;
-            }
-            catch (SQLiteException error)
-            {
-                SqlErrorMessage = error.Message.ToString();
-                SqlErrorNum = error.ErrorCode;
-                SqlError = true;
-            }
-            finally
-            {
-                SqlAdapter.Dispose();
-                Connection.Close();
-            }
 
             return data;
         }
